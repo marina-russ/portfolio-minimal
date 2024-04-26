1b1ca2a09a3c7c6a0fc167a16854749df61b2c28
Marina Russ <mrusscode@gmail.com>
2024-04-26 15:24:36 -0400

    updates website URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://www.marinaruss.dev", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "Marina Russ | Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Marina Russ | Developer Portfolio", // Used in manifest.json
+          shortName: "Developer Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
